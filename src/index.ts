435676fbd994b6265d1db2454c787de655d935fa
Borja <pintocorreas@gmail.com>
2023-10-18 13:16:06 +0200

    feat: create 4 endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,26 @@ import express from "express"
 
 const app = express()
 
+app.get("/films", (req, res) => {
+
+    return res.send("Get films") 
+})
+
+app.post("/films", (req, res)=> {
+
+    return res.send("CREATE FILM")
+})
+
+app.put("/films", (req, res)=> {
+
+    return res.send("UPDATE FILM")
+})
+
+app.delete("/films", (req, res)=> {
+
+    return res.send("DELETE FILM")
+})
+
 app.listen(4000, () =>{
     console.log("Server running");
     
